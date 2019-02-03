11038f07f3ceed344ebef9d3ad3e03666374200b
Lathe Abusaid <labusaid@gmail.com>
2019-02-03 08:32:28 -0600

    Added resume prefab

---
+++ HackSMU/Assets/Scripts/Resume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Resume : MonoBehaviour {
+
+    private GameObject resume;
+
+    public void ResumePopup()
+    {
+        resume.SetActive(true);
+    }
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(resume, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
